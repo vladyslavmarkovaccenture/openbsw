ea78cd31e907bb052f0a78fc5cb42b1354c7d3a7
Nikita Belenkiy <nikita.belenkiy@accenture.com>
2024-11-27 13:25:55 +0100

    Add compiler barriers to S32K1 interrupts
    Change-Id: I4715c4876beb298315b849940ee50e8010620515

--- platforms/s32k1xx/bsp/bspInterruptsImpl/include/interrupts/suspendResumeAllInterrupts.h
+++ platforms/s32k1xx/bsp/bspInterruptsImpl/include/interrupts/suspendResumeAllInterrupts.h
@@ -16,17 +16,17 @@ static inline __attribute__((always_inline))
 uint32_t getMachineStateRegisterValueAndSuspendAllInterrupts(void)
 {
    uint32_t _PRIMASK;
-   __asm("    mrs     %0, PRIMASK\n"
-         "    cpsid   i\n"
-         : "=r" (_PRIMASK));
+   __asm volatile ("    mrs     %0, PRIMASK\n"
+                   "    cpsid   i\n"
+                   : "=r" (_PRIMASK));
     return(_PRIMASK);
 }
 static inline __attribute__((always_inline))
 void resumeAllInterrupts(uint32_t oldMachineStateRegisterValue)
 {
-    __asm("      msr     PRIMASK,%[Input]\n"
-          ::[Input] "r" (oldMachineStateRegisterValue)
-        );
+    __asm volatile ("      msr     PRIMASK,%[Input]\n"
+                    ::[Input] "r" (oldMachineStateRegisterValue)
+                  );
 }
 
 // clang-format on
