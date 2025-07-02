e2c6dad5f39ffdb11f1689f0d40c9e42e3b8eca2
Roland Reichwein <Roland.Reichwein@bmw.de>
2025-07-02 07:14:49 +0000

    Remove dependency on std type_traits headers

--- libs/bsw/uds/src/uds/services/routinecontrol/StartRoutine.cpp
+++ libs/bsw/uds/src/uds/services/routinecontrol/StartRoutine.cpp
@@ -6,13 +6,13 @@
 #include "uds/session/DiagSession.h"
 #include "uds/session/IDiagSessionManager.h"
 
-#include <type_traits>
+#include <etl/type_traits.h>
 
 namespace uds
 {
 uint8_t const StartRoutine::sfImplementedRequest[2]
     = {ServiceId::ROUTINE_CONTROL,
-       static_cast<::std::underlying_type<RoutineControl::Subfunction>::type>(
+       static_cast<::etl::underlying_type<RoutineControl::Subfunction>::type>(
            RoutineControl::Subfunction::START_ROUTINE)};
 
 StartRoutine::StartRoutine() : Subfunction(&sfImplementedRequest[0], DiagSession::ALL_SESSIONS())
--- libs/bsw/uds/src/uds/services/routinecontrol/StopRoutine.cpp
+++ libs/bsw/uds/src/uds/services/routinecontrol/StopRoutine.cpp
@@ -6,13 +6,13 @@
 #include "uds/session/DiagSession.h"
 #include "uds/session/IDiagSessionManager.h"
 
-#include <type_traits>
+#include <etl/type_traits.h>
 
 namespace uds
 {
 uint8_t const StopRoutine::sfImplementedRequest[2]
     = {ServiceId::ROUTINE_CONTROL,
-       static_cast<::std::underlying_type<RoutineControl::Subfunction>::type>(
+       static_cast<::etl::underlying_type<RoutineControl::Subfunction>::type>(
            RoutineControl::Subfunction::STOP_ROUTINE)};
 
 StopRoutine::StopRoutine() : Subfunction(&sfImplementedRequest[0], DiagSession::ALL_SESSIONS())
